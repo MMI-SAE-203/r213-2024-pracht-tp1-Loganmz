d78cb23bed7f282ac90cf2c6882cedb5e4302e57
Loganmz <116796944+Loganmz@users.noreply.github.com>
2024-04-11 16:42:40 +0200

    boucle

--- src/App.vue
+++ src/App.vue
@@ -11,27 +11,32 @@ const menuIsOpen = ref(false)
 </script>
 
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li>
-          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-          <RouterLink to="/accordeon" class="text-red-500 underline" active-class="text-blue-600 underline"> Accordéon
-          </RouterLink>
-          <RouterLink to="/boucles" class="text-red-500 underline" active-class="text-blue-600 underline">Boucles
-          </RouterLink>
-
+  <header >
+    <nav class="bg-sky-950">
+      <ul class="flex space-x-80 justify-center">
+        <li class="p-4  ">
+          <RouterLink to="/" class=" text-emerald-300 underline"> Accueil </RouterLink>
         </li>
+        <li class="p-4 ">
+          <RouterLink to="/accordeon" class=" text-emerald-300 underline" active-class="text-blue-600 underline"> Accordéon
+          </RouterLink>
+        </li>
+        <li class="p-4">
+          <RouterLink to="/boucles" class=" text-emerald-300 underline" active-class="text-blue-600 underline">Boucles
+          </RouterLink>
+        </li>
+        <button @pointerdown="menuIsOpen = !menuIsOpen" aria-controls="mainNav" aria-expanded="true" class="rounded-full border-2 border-red-600 bg-red-300 px-2">menu
+          </button>
       </ul>
+          
+  
     </nav>
   </header>
-  <button @pointerdown="menuIsOpen = !menuIsOpen" aria-controls="mainNav" aria-expanded="true" class="rounded-full border-2 border-red-600 bg-red-300 px-2">
-    menu
-  </button>
+  
   <!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
   <Transition class="transition-transform duration-1000" enter-from-class="-translate-x-full"
     enter-to-class="translate-x-0" leave-active-class="-translate-x-full">
-    <nav id="mainNav" v-show="menuIsOpen" class="bg-sky-950 w-1/6 text-emerald-300 flex justify-center py-10">
+    <nav id="mainNav" v-show="menuIsOpen" class=" w-screen bg-sky-950 text-emerald-300 flex justify-center py-10">
       <ul>
         <li >
           <RouterLink to="/"> Accueil </RouterLink>
--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -8,14 +8,14 @@ const sectionOpen = ref(1)
     <p>Accordeon</p>
     <section>
         <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 1 ? 0 : 1">bouton 1</button>
-        <p v-show="sectionOpen ===1">loren...</p>
+        <p v-show="sectionOpen ===1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repudiandae ea sunt deserunt quidem illo aut assumenda atque, aspernatur nulla, tempore esse iusto dolore aliquid animi! Voluptate blanditiis ullam aliquid?</p>
     </section>
     <section>
         <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 2 ? 0 : 2">bouton 2</button>
-        <p v-show="sectionOpen ===2">loren...</p>
+        <p v-show="sectionOpen ===2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis dolorum ex repellendus repellat vitae! Deleniti dolore pariatur animi quaerat sequi saepe. Ad similique animi molestias dolorum atque. Itaque, cum fugit!</p>
     </section>
     <section>
         <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 3 ? 0 : 3">bouton 3</button>
-        <p v-show="sectionOpen ===3">loren...</p>
+        <p v-show="sectionOpen ===3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit vero voluptatum consectetur perspiciatis cum autem, at inventore animi. Modi exercitationem velit ad porro, inventore amet necessitatibus eos? Doloremque, quisquam harum?</p>
     </section>
 </template>
