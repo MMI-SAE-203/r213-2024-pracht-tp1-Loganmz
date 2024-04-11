385b96b7ad75d780a19efb90f3b41894c013ee3b
Loganmz <116796944+Loganmz@users.noreply.github.com>
2024-04-11 16:21:32 +0200

    autre section

--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -5,9 +5,17 @@ const sectionOpen = ref(1)
 </script>
 
 <template>
-    <p>Onglet 1</p>
+    <p>Accordeon</p>
     <section>
-    <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 1 ? 0 : 1">bouton 1</button>
-    <p v-show="sectionOpen ===1">loren...</p>
-  </section>
+        <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 1 ? 0 : 1">bouton 1</button>
+        <p v-show="sectionOpen ===1">loren...</p>
+    </section>
+    <section>
+        <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 2 ? 0 : 2">bouton 2</button>
+        <p v-show="sectionOpen ===2">loren...</p>
+    </section>
+    <section>
+        <button class="text-xl" @pointerdown="sectionOpen = sectionOpen === 3 ? 0 : 3">bouton 3</button>
+        <p v-show="sectionOpen ===3">loren...</p>
+    </section>
 </template>
